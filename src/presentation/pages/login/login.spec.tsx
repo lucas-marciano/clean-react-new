2a33579ab44a0ac76bd569ade9bc0db524af1660
Lucas de Almeida Marciano <lucas.marciano@invillia.com>
2020-12-16 12:34:47 -0300

    test: initial state of the login

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,7 +3,26 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login components', () => {
-  test('Shold not show the loading and error message when initialize the view', () => {
+  test('Shold start with initial state of the inputs', () => {
+    const { getByTestId } = render(<Login />)
+
+    const emailStatus = getByTestId('email-status') as HTMLButtonElement
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+
+    const passwordStatus = getByTestId('password-status') as HTMLButtonElement
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('Shold start with initial state of the button', () => {
+    const { getByTestId } = render(<Login />)
+    const submit = getByTestId('submit') as HTMLButtonElement
+
+    expect(submit.disabled).toBe(true)
+  })
+
+  test('Shold not render the loading and error message when initialize the view', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
 
