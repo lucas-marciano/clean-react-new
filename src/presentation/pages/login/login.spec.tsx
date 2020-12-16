00708f4302abe2d40347e7a4d1f6a4df2df7737c
Lucas de Almeida Marciano <lucas.marciano@invillia.com>
2020-12-16 11:46:42 -0300

    test: ensure the context for the login page

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Login from './login'
 
-describe('', () => {
-  test('', () => {
-    render(<Login />)
+describe('Login components', () => {
+  test('Shold not show the loading and error message when initialize the view', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrap = getByTestId('error-wrap')
+
+    expect(errorWrap.childElementCount).toBe(0)
   })
 })
