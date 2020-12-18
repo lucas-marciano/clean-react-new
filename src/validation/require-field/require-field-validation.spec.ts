fdcb41eab470b12f246a9033f71a386f36aeda58
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-18 13:52:15 -0300

    test: ensure the validation for the field empty

--- src/validation/require-field/require-field-validation.spec.ts
+++ src/validation/require-field/require-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import RequireFieldValidation from '@/validation/require-field/require-field-validation'
 import { RequireFieldError } from '@/validation/error'
+import faker from 'faker'
 
 describe('RequireFieldValidation', () => {
   test('Should have error if field is empty', () => {
     const sut = new RequireFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequireFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequireFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
