8bdae8c5cf1dc52051306f41d968169e5f8494f0
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-18 14:08:45 -0300

    refactor: create a factory sut

--- src/validation/require-field/require-field-validation.spec.ts
+++ src/validation/require-field/require-field-validation.spec.ts
@@ -2,16 +2,33 @@ import RequireFieldValidation from '@/validation/require-field/require-field-val
 import { RequireFieldError } from '@/validation/error'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: RequireFieldValidation
+  error: Error
+}
+
+type SutParam = {
+  field: string
+  value: string
+}
+
+const makeSut = (params: SutParam): SutTypes => {
+  const sut = new RequireFieldValidation(params.field)
+  const error = sut.validate(params.value)
+  return {
+    sut,
+    error
+  }
+}
+
 describe('RequireFieldValidation', () => {
   test('Should have error if field is empty', () => {
-    const sut = new RequireFieldValidation('email')
-    const error = sut.validate('')
+    const { error } = makeSut({ field: 'email', value: '' })
     expect(error).toEqual(new RequireFieldError())
   })
 
   test('Should return falsy if field is not empty', () => {
-    const sut = new RequireFieldValidation('email')
-    const error = sut.validate(faker.random.word())
+    const { error } = makeSut({ field: 'email', value: faker.random.word() })
     expect(error).toBeFalsy()
   })
 })
