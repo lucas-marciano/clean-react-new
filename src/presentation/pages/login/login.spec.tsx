049ed548f7cabb42a5a81c2ae7684602953e96ce
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-18 08:48:30 -0300

    test: ensure that form authentication was not calling when be a error

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -130,4 +130,12 @@ describe('Login components', () => {
     simulateValidSubmit(sut)
     expect(authenticationSpy.callsCount).toBe(1)
   })
+
+  test('Shold not call Authrentication is form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('login-form'))
+    expect(authenticationSpy.callsCount).toBe(0)
+  })
 })
