cf2bd724ce84a29c9f359ddec939d32c2d2a1815
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-21 11:21:42 -0300

    refactor: create a SUT factory

--- src/validation/validators/composite/composite-valitaion.spec.ts
+++ src/validation/validators/composite/composite-valitaion.spec.ts
@@ -1,16 +1,28 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { CompositeValidation } from './composite-valitaion'
 
+type SutTypes = {
+  sut: CompositeValidation
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new CompositeValidation(fieldValidationsSpy)
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('CompositeValidation', () => {
   test('should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error('first_error_message')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('second_error_message')
-    const sut = new CompositeValidation([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('first_error_message')
+    fieldValidationsSpy[1].error = new Error('second_error_message')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('first_error_message')
   })
