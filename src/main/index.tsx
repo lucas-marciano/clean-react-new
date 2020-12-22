1662b219138a43794065833d68c7c421dbf26118
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-22 10:31:04 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import '@/presentation/styles/globals.scss'
 
 ReactDOM.render(
-  <Router />,
+  <Router
+    makeLogin={makeLogin}
+  />,
   document.getElementById('main')
 )
