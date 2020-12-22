5416726198ae5e462ba4736fc5b1074beea7f630
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-22 09:18:45 -0300

    refactor: create a index for the validators folder

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,6 @@
+export * from './email/email-validation'
+export * from './min-length/min-length-validation'
+export * from './require-field/require-field-validation'
+export * from './composite/composite-valitaion'
+export * from './require-field/require-field-validation'
+export * from './builder/validation-builder'
