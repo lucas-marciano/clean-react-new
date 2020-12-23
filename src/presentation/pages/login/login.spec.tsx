31cf21d91eebac80b78c82b7080526d3359d3a7b
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-23 15:02:47 -0300

    chore: add files to jest ignore

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,7 +6,7 @@ import 'jest-localstorage-mock'
 import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react'
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test'
 import { InvalidCredentialsError } from '@/domain/errors'
-import Login from './login'
+import { Login } from '@/presentation/pages'
 
 type SutTypes = {
   sut: RenderResult
