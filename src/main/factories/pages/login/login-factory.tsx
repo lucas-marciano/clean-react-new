2f83a4fdea45d9e183d9c44f3aa3df3b410569c2
Lucas Marciano <lucas.marciano@invillia.com>
2020-12-30 09:02:24 -0300

    feat: change Login composition

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Login } from '@/presentation/pages'
 import { makeRemoteAuth } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 import { makeValidation } from './login-validation-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-access-token-factory'
+import { Login } from '@/presentation/pages'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuth()}
       validation={makeValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   )
 }
