9fec4ddc4515b5eb2e580c447526b97aceaebca1
Lucas Marciano <lucas.marciano@invillia.com>
2021-07-27 11:12:59 -0300

    refactor: add SubmitButton component

--- src/presentation/pages/sigup/sigup.tsx
+++ src/presentation/pages/sigup/sigup.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import Styles from './sigup-styles.scss'
-import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components/'
+import { Footer, FormStatus, Input, LoginHeader, SubmitButton } from '@/presentation/components/'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { AddAccount, SaveAccessToken } from '@/domain/usecases'
@@ -16,6 +16,7 @@ const SigUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pro
   const history = useHistory()
   const [state, setState] = useState({
     isLoading: false,
+    isFormInvalid: true,
     name: '',
     email: '',
     password: '',
@@ -30,11 +31,7 @@ const SigUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pro
   const hanldeSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     try {
-      if (state.isLoading ||
-      state.nameError ||
-      state.emailError ||
-      state.passwordError ||
-      state.passwordConfirmationError) { return }
+      if (state.isLoading || state.isFormInvalid) return
 
       setState({ ...state, isLoading: true })
       const account = await addAccount.add({
@@ -55,12 +52,18 @@ const SigUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pro
   }
 
   useEffect(() => {
+    const nameError = validation.validate('name', state.name)
+    const emailError = validation.validate('email', state.email)
+    const passwordError = validation.validate('password', state.password)
+    const passwordConfirmationError = validation.validate('passwordConfirmation', state.passwordConfirmation)
+
     setState({
       ...state,
-      nameError: validation.validate('name', state.name),
-      emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password),
-      passwordConfirmationError: validation.validate('passwordConfirmation', state.passwordConfirmation)
+      nameError,
+      emailError,
+      passwordError,
+      passwordConfirmationError,
+      isFormInvalid: !!nameError || !!emailError || !!passwordError || !!passwordConfirmationError
     })
   }, [state.name, state.email, state.password, state.passwordConfirmation])
 
@@ -74,12 +77,7 @@ const SigUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pro
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Repita a senha" />
-          <button data-testid="submit" disabled={
-            !!state.nameError ||
-            !!state.emailError ||
-            !!state.passwordConfirmationError ||
-            !!state.passwordError
-          } className={Styles.submit} type="submit">Criar</button>
+          <SubmitButton text="Cadastrar no sistema"/>
           <Link data-testid="login-link" replace to="/login" className={Styles.link}>Voltar para o Login</Link>
           <FormStatus />
         </form>
